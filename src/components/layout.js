d7bef1eea380fb45ab2c1a30397eb32c02d15928
anhld <wolfwithcode@gmail.com>
2020-07-16 17:37:46 +0700

    Add footer componnent

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Menu from "../components/Menu"
+import Footer from "../components/Footer"
 
 import "./layout.css"
 
@@ -37,11 +38,9 @@ const Layout = ({ children }) => {
         // }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer>
+
+        </Footer>
       </div>
     </>
   )
