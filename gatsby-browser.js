c725e24ad7724d2110aad94b26b1d47f965a699d
anhld <wolfwithcode@gmail.com>
2022-09-03 10:38:07 +0700

    fix price circle

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,4 @@
 
 // You can delete this file if you're not using it
 import "./node_modules/bootstrap/dist/css/bootstrap.css"
+import "./src/styles/global.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         includeInDevelopment: false,
       },
     },
+    "gatsby-plugin-postcss",
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
